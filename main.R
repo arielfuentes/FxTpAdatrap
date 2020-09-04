a0ca38eae744f663918644acd81b0409c2c62a1a
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2020-09-04 12:21:05 -0400

    Update main.R

--- main.R
+++ main.R
@@ -1,3 +1,8 @@
+#libraries
+library(purrr)
+library(readr)
+library(dplyr)
+
 #parameters
 file_par <- "data/2019-07-06_consolidado_anexo4_(Circunvalación)_anual.xlsx"
 file_nd <- "data/dicc_nodoV2_2.csv"
@@ -6,6 +11,18 @@ source(file = "code/ipt.R", encoding = "utf-8")
 source(file = "code/vjs_stpfix.R", encoding = "utf-8")
 source(file = "code/wrg_dt.R", encoding = "utf-8")
 DDBB_v <- "[viajes201908]"
-raw_vjs <- vjs_stpfix(DDBB_v, per)
 
-lapply(X = per$periodomediodeviaje, FUN = function(x) vjs_fix(raw_vjs, x))
+lapply(X = per$periodomediodeviaje, 
+       FUN = function(x) vjs_fix(vjs_stpfix(df = BBDD_vjs(DDBB_v, x)
+                                           ),
+                                 x)
+       )
+
+g <- list.files(path = "output/loss_rep/", full.names = T)
+
+g %>%
+  purrr::map(function(x) {
+    read_delim(x, delim = ";")
+  }) %>%
+  reduce(bind_rows) %>%
+  write_delim(path = "output/loss_rep/loss_rep_082019.csv", delim = ";")
